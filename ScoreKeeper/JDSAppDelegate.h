5b089a4b36a0f0cc1d15cb03c4f70a7afaea1e2f
John D. Storey <johndangerstorey@gmail.com>
2014-05-21 19:02:35 -0600

    Initial Commit

---
+++ ScoreKeeper/JDSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JDSAppDelegate.h
+//  ScoreKeeper
+//
+//  Created by John D. Storey on 5/21/14.
+//  Copyright (c) 2014 d. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ScoreKeeper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ScoreKeeper
+//
+//  Created by John D. Storey on 5/21/14.
+//  Copyright (c) 2014 d. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JDSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JDSAppDelegate class]));
+    }
+}
